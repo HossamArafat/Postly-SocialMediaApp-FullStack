a9267f35bef8a541fa4307bdf2b3bde39547b05c
HossamArafat <hussam67215@gmail.com>
2025-11-21 21:08:11 +0200

    fix inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -12,7 +12,7 @@ export const inngest = new Inngest({ id: "postly", eventKey: process.env.INNGEST
 // Inngest function to save user of clerk data to a database
 const syncUserCreation = inngest.createFunction(
     {id: "sync-user-from-clerk"},
-    {event: ["clerk/user.created", "user.created"]},
+    {event: "clerk/user.created"},
     async ({event, step})=> {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const email = email_addresses[0].email_address
